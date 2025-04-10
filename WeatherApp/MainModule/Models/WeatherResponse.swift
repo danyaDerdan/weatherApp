86cf2449104555a62e41915c337d6d0b55c5c751
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-04-10 11:24:22 +0300

    Added networkService

--- WeatherApp/MainModule/Models/WeatherResponse.swift
+++ WeatherApp/MainModule/Models/WeatherResponse.swift
@@ -1,7 +1,20 @@
-//
-//  WeatherResponse.swift
-//  WeatherApp
-//
-//  Created by Данил Толстиков on 10.04.2025.
-//
-
+struct WeatherResponse: Decodable {
+    
+    let location: Location
+    let current: Current
+    
+    struct Location: Codable {
+        let name: String
+        let localtime: String
+    }
+    
+    struct Current: Codable {
+        let temp_c: Double
+        let condition: Condition
+    }
+    
+    struct Condition: Codable {
+        let text: String
+        let icon: String
+    }
+}
--- WeatherApp/Services/NetworkService.swift
+++ WeatherApp/Services/NetworkService.swift
@@ -1,7 +1,35 @@
-//
-//  NetworkService.swift
-//  WeatherApp
-//
-//  Created by Данил Толстиков on 10.04.2025.
-//
+import Foundation
 
+protocol NetworkServiceProtocol {
+    func fetchWeatherData(for city: String,  stringUrl: String, completion: @escaping (Result<WeatherResponse, Error>) -> Void)
+    func fetchImage(stringUrl: String, complition: @escaping ((Result<Data, Error>) -> Void))
+}
+
+
+final class NetworkService: NetworkServiceProtocol {
+    func fetchWeatherData(for city: String,  stringUrl: String, completion: @escaping (Result<WeatherResponse, Error>) -> Void) {
+        guard let url = URL(string: stringUrl) else { completion(.failure(NetworkError.urlError)); return }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error { completion(.failure(error)) }
+            guard let data else { completion(.failure(NetworkError.badData)); return }
+            let decoder = JSONDecoder()
+            guard let result = try? decoder.decode(WeatherResponse.self, from: data) else { completion(.failure(NetworkError.decodingError)); return}
+            completion(.success(result))
+        }.resume()
+    }
+    
+    func fetchImage(stringUrl: String, complition: @escaping ((Result<Data, Error>) -> Void)) {
+        guard let url = URL(string: stringUrl) else { complition(.failure(NetworkError.urlError)); return }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error { complition(.failure(error)) }
+            guard let data else { complition(.failure(NetworkError.badData)); return }
+            complition(.success(data))
+        }.resume()
+    }
+}
+
+enum NetworkError: Error {
+    case urlError
+    case decodingError
+    case badData
+}
